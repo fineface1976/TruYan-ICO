24654333c6cf090daf9e0c2980678a286ed9a04c
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-25 21:16:37 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    bscmainnet: {
+      url: process.env.BSC_MAINNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY
+  }
+};
